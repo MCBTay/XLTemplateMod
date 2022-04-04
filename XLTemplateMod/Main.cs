adf14e3fe3be3a59461394ceb614b347138ec737
mcbtay <mcbtay@gmail.com>
2022-04-04 00:13:12 -0400

    moving files to proper file structure, adding main/settings/info.json

---
+++ XLTemplateMod/Main.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace XLPrecisionKeyframes
+{
+#if DEBUG
+    [EnableReloading]
+#endif
+    static class Main
+    {
+        public static bool Enabled;
+        private static Harmony Harmony;
+
+        public static GameObject UserInterfaceGameObject;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Instance = UnityModManager.ModSettings.Load<Settings>(modEntry);
+            Settings.ModEntry = modEntry;
+
+            UserInterfaceGameObject = new GameObject();
+            UserInterfaceGameObject.SetActive(false);
+            UserInterfaceGameObject.AddComponent<UserInterface.UserInterface>();
+            Object.DontDestroyOnLoad(UserInterfaceGameObject);
+
+            modEntry.OnToggle = OnToggle;
+#if DEBUG
+            modEntry.OnUnload = Unload;
+#endif
+
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if (Enabled == value) return true;
+            Enabled = value;
+
+            if (Enabled)
+            {
+                Harmony = new Harmony(modEntry.Info.Id);
+                Harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                Object.DestroyImmediate(UserInterfaceGameObject);
+                Harmony.UnpatchAll(Harmony.Id);
+            }
+
+            return true;
+        }
+
+#if DEBUG
+        static bool Unload(UnityModManager.ModEntry modEntry)
+        {
+            Object.DestroyImmediate(UserInterfaceGameObject);
+
+            Harmony?.UnpatchAll();
+            return true;
+        }
+#endif
+    }
+}
---
+++ XLTemplateMod/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityModManagerNet;
+
+namespace XLPrecisionKeyframes
+{
+    [Serializable]
+    public class Settings : UnityModManager.ModSettings
+    {
+        public static Settings Instance { get; set; }
+        public static UnityModManager.ModEntry ModEntry;
+
+        public Settings() : base()
+        {
+
+        }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+
+        public void Save()
+        {
+            Save(ModEntry);
+        }
+    }
+}
